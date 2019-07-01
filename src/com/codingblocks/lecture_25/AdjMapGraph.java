6f6419f66358ca862b0b57c6484cf58675d8483c
Keenwarrior <anujgargcse@gmail.com>
2019-07-01 10:44:07 +0530

    l 25

--- src/com/codingblocks/lecture_25/AdjMapGraph.java
+++ src/com/codingblocks/lecture_25/AdjMapGraph.java
@@ -13,14 +13,67 @@ public class AdjMapGraph<E> {
         }
     }
 
-    public void addEdge(E s_value, E e_value){
+    public void addEdge(E s_value, E e_value, int weight){
         Vertex start = vertexMap.get(s_value);
         Vertex end = vertexMap.get(e_value);
 
         if (start!=null && end!= null){
-            start.addNeighbour(e_value, end);
-            end.addNeighbour(s_value, start);
+            start.addNeighbour(e_value, end, weight);
+            end.addNeighbour(s_value, start, weight);
         }
+    }
+
+    public int minSpanningTree(){
+        ArrayList<Edge> list = new ArrayList<>();
+        for (Vertex start : vertexMap.values()) {
+            for (Vertex end : start.neighbours.values()) {
+                int weight = start.weights.get(end.value);
+                list.add(new Edge(start, end, weight));
+            }
+        }
+
+        list.sort((o1, o2) -> o1.weight - o2.weight);
+
+        Map<E, E> parents = populateParents();
+
+        int result = 0;
+
+        for (Edge e : list) {
+            if (union(e.start.value, e.end.value, parents)){
+                result += e.weight;
+            }
+        }
+
+        return result;
+
+    }
+
+    public AdjMapGraph<E> minSpanningTreeGraph(){
+        ArrayList<Edge> list = new ArrayList<>();
+        for (Vertex start : vertexMap.values()) {
+            for (Vertex end : start.neighbours.values()) {
+                int weight = start.weights.get(end.value);
+                list.add(new Edge(start, end, weight));
+            }
+        }
+
+        list.sort((o1, o2) -> o1.weight - o2.weight);
+
+        Map<E, E> parents = populateParents();
+
+        AdjMapGraph<E> spanning = new AdjMapGraph<>();
+        for (E element : vertexMap.keySet()) {
+            spanning.addVertex(element);
+        }
+
+        for (Edge e : list) {
+            if (union(e.start.value, e.end.value, parents)){
+                spanning.addEdge(e.start.value, e.end.value, e.weight);
+            }
+        }
+
+        return spanning;
+
     }
 
     public void display(){
@@ -33,17 +86,58 @@ public class AdjMapGraph<E> {
         }
     }
 
+    private Map<E, E> populateParents(){
+        Map<E, E> map = new HashMap<>();
+        for (E key : vertexMap.keySet()) {
+            map.put(key, null);
+        }
+        return map;
+    }
+
+    private E find(E element, Map<E, E> parents){
+        while (parents.get(element) != null){
+            element = parents.get(element);
+        }
+        return element;
+    }
+
+    private boolean union(E first, E second, Map<E, E> parents){
+        first = find(first, parents);
+        second = find(second, parents);
+
+        if (!first.equals(second)){
+            parents.put(first, second);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private class Edge{
+        Vertex start;
+        Vertex end;
+        int weight;
+
+        public Edge(Vertex start, Vertex end, int weight) {
+            this.start = start;
+            this.end = end;
+            this.weight = weight;
+        }
+    }
 
     private class Vertex {
         E value;
         Map<E, Vertex> neighbours;
+        Map<E, Integer> weights;
         public Vertex(E value) {
             this.value = value;
             this.neighbours = new HashMap<>();
+            this.weights = new HashMap<>();
         }
 
-        public void addNeighbour(E value, Vertex vertex){
+        public void addNeighbour(E value, Vertex vertex, int weight){
             this.neighbours.put(value, vertex);
+            this.weights.put(value, weight);
         }
     }
 }
---
+++ src/com/codingblocks/lecture_25/Client.java
@@ -0,0 +1,28 @@
+package com.codingblocks.lecture_25;
+
+import java.util.Calendar;
+
+public class Client {
+    public static void main(String[] args) {
+        AdjMapGraph<Character> graph = new AdjMapGraph<>();
+
+        graph.addVertex('a');
+        graph.addVertex('b');
+        graph.addVertex('c');
+        graph.addVertex('d');
+        graph.addVertex('e');
+
+        graph.addEdge('a', 'e', 1);
+        graph.addEdge('a', 'b', 2);
+        graph.addEdge('a', 'd', 3);
+        graph.addEdge('e', 'd', 4);
+        graph.addEdge('b', 'c', 5);
+        graph.addEdge('e', 'c', 7);
+
+        AdjMapGraph<Character> spanning = graph.minSpanningTreeGraph();
+
+        graph.display();
+        System.out.println("--------------");
+        spanning.display();
+    }
+}
